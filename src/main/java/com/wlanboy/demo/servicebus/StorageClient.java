33bedb609a835b57c3ebd58fec0dce9d7d211aef
wlanboy <me@wlanboy.com>
2018-09-16 07:14:21 +0200

    Added Azure Storage client.

---
+++ src/main/java/com/wlanboy/demo/servicebus/StorageClient.java
@@ -0,0 +1,104 @@
+package com.wlanboy.demo.servicebus;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.microsoft.applicationinsights.core.dependencies.apachecommons.io.FilenameUtils;
+import com.microsoft.azure.storage.CloudStorageAccount;
+import com.microsoft.azure.storage.StorageException;
+import com.microsoft.azure.storage.blob.CloudBlobClient;
+import com.microsoft.azure.storage.blob.CloudBlobContainer;
+import com.microsoft.azure.storage.blob.CloudBlockBlob;
+import com.microsoft.azure.storage.blob.ListBlobItem;
+
+@Service
+public class StorageClient {
+
+	private static final Logger logger = Logger.getLogger(StorageClient.class.getCanonicalName());
+
+	@Autowired
+	private CloudStorageAccount cloudStorageAccount;
+
+	final String containerName = "azurestorage";
+
+	public StorageClient() {
+
+	}
+
+	public void createContainerIfNotExists() throws URISyntaxException, StorageException {
+		try {
+			final CloudBlobClient blobClient = cloudStorageAccount.createCloudBlobClient();
+			final CloudBlobContainer container = blobClient.getContainerReference(containerName);
+
+			container.createIfNotExists();
+		} catch (Exception e) {
+			logger.severe(e.getMessage());
+		}
+	}
+
+	public void uploadBlob(String filename, InputStream file, long length) throws URISyntaxException, StorageException {
+		try {
+
+			final CloudBlobClient blobClient = cloudStorageAccount.createCloudBlobClient();
+			final CloudBlobContainer container = blobClient.getContainerReference(containerName);
+			CloudBlockBlob blob = container.getBlockBlobReference(filename);
+			blob.upload(file, length);
+
+		} catch (Exception e) {
+			logger.severe(e.getMessage());
+		}
+	}
+
+	public ArrayList<String> ListContainerBlobs() {
+		ArrayList<String> files = new ArrayList<String>();
+
+		try {
+
+			final CloudBlobClient blobClient = cloudStorageAccount.createCloudBlobClient();
+			final CloudBlobContainer container = blobClient.getContainerReference(containerName);
+
+			for (ListBlobItem blobItem : container.listBlobs()) {
+				files.add(FilenameUtils.getName(blobItem.getUri().getPath()));
+			}
+
+		} catch (Exception e) {
+			logger.severe(e.getMessage());
+		}
+		return files;
+	}
+
+	public OutputStream downloadBlob(String filename, OutputStream download)
+			throws URISyntaxException, StorageException {
+
+		try {
+
+			final CloudBlobClient blobClient = cloudStorageAccount.createCloudBlobClient();
+			final CloudBlobContainer container = blobClient.getContainerReference(containerName);
+			CloudBlockBlob blob = container.getBlockBlobReference(filename);
+			blob.download(download);
+		} catch (Exception e) {
+			logger.severe(e.getMessage());
+		}
+
+		return download;
+	}
+
+	public void deleteBlob(String filename) throws URISyntaxException, StorageException {
+		try {
+
+			final CloudBlobClient blobClient = cloudStorageAccount.createCloudBlobClient();
+			final CloudBlobContainer container = blobClient.getContainerReference(containerName);
+			CloudBlockBlob blob = container.getBlockBlobReference(filename);
+			blob.deleteIfExists();
+		} catch (Exception e) {
+			logger.severe(e.getMessage());
+		}
+	}
+
+}
